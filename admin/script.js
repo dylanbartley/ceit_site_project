b49bdf814cf2d842e3e79da6b9322020e4c31d0c
unknown <d.bartley44@gmail.com>
2017-08-22 20:25:31 -0400

    updated admin files

--- admin/script.js
+++ admin/script.js
@@ -1,5 +1,9 @@
 $(function () {
     /* wire header buttons */
+    $('.ctl-btn').click(function ( e) {
+        var page = $(e.currentTarget).attr('data-target');
+        $('#main').load("partial/" + page + ".phtml");
+    });
     
     /* load batches as default */
     $('#main').load("partial/batches.phtml");
